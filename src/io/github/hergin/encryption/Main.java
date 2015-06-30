5134f5eee7d52570290ac8fc610fec6379cfbf94
hergin <hergin@crimson.ua.edu>
2015-06-30 18:46:27 -0500

    PrimerPair

--- src/io/github/hergin/encryption/Main.java
+++ src/io/github/hergin/encryption/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i=0;
-		
-		int j=0;
-		
-		int y=0;
-		double x=0;
 		
 	}
 
---
+++ src/io/github/hergin/encryption/PrimePair.java
@@ -0,0 +1,14 @@
+package io.github.hergin.encryption;
+
+public class PrimePair {
+
+	long q;
+
+	long p;
+
+	public PrimePair(long p, long q) {
+		this.q = q;
+		this.p = p;
+	}
+
+}
